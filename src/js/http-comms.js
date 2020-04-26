bc9f749179cf2c7c1a2c06fb92ab9af6a3b174b2
Josh Adams <joshua.william.adams@gmail.com>
2020-04-26 08:22:34 +0800

    Create http-comms.js
    http communications module.

---
+++ src/js/http-comms.js
@@ -0,0 +1,63 @@
+/*!
+ * HTTP Communications
+ * Standardised HTTP communications functions
+ * (c) 2020 Joshua Adams
+ */
+
+/* ============================== Import Modules ============================ */
+
+// N/A
+
+/* ================================ Variables =============================== */
+
+// N/A
+
+/* ============================= Private Methods ============================ */
+
+function _appendRequestHeaders(xhr, headers) {
+  for (let key in headers) {
+    if (headers.hasOwnProperty(key)) {
+      if (typeof headers[key] === 'object') {
+        xhr.setRequestHeader(key, JSON.stringify(headers[key]));
+      } else {
+        xhr.setRequestHeader(key, headers[key]);
+      }
+    }
+  }
+}
+
+/* ============================== Public Methods ============================ */
+
+function sendRequest(options) {
+  // define AJAX request configuration
+  const cnf = {
+    method: options.method || 'GET'
+    , url: options.url
+    , payload: options.payload
+    , headers: options.headers
+  }
+  // send request
+  return new Promise (function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(cnf.method, cnf.url, true);
+    // add additional headers to payload
+    _appendRequestHeaders(xhr, headers);
+    // send request
+    xhr.send(payload);
+    // handle request reponse
+    xhr.onreadystatechange = function() {
+      // 4 = done
+      if (xhr.readyState === 4) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.response);
+      }
+    }
+  })
+}
+
+/* =========================== Export Public APIs =========================== */
+
+export default {
+  sendRequest
+};
